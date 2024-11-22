7b0434a94760a626e28907ff4cb8b6d15faacf2c
nsankethreddy <sankethn1947@gmail.com>
2024-11-22 17:58:29 -0500

    man

---
+++ Lab5/WorkingWithObjects.js
@@ -0,0 +1,43 @@
+const assignment = {
+    id: 1, title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-10-10", completed: false, score: 0,
+  };
+const module={
+    id:1, name:"NodeJS Module",description:"Module explaining how to create a NodeJS with ExpressJS", course:"MERN Stack"
+}
+  export default function WorkingWithObjects(app) {
+    app.get("/lab5/assignment", (req, res) => {
+      res.json(assignment);
+    });
+    app.get("/lab5/assignment/title", (req, res) => {
+        res.json(assignment.title);
+      });
+    app.get("/lab5/assignment/title/:newTitle", (req, res) => {
+        const { newTitle } = req.params;
+        assignment.title = newTitle;
+        res.json(assignment);
+    });
+    app.get("/lab5/assignment/score/:newScore", (req, res) => {
+        const { newScore } = req.params;
+        assignment.score = newScore;
+        res.json(assignment);
+    });
+    app.get("/lab5/assignment/completed/:newStatus", (req, res) => {
+        const { newStatus } = req.params;
+        assignment.completed = newStatus;
+        res.json(assignment);
+    });
+    app.get("/lab5/module", (req, res) => {
+        res.json(module);
+    });
+    app.get("/lab5/module/name", (req, res) => {
+        res.json(module.name);
+    });
+    app.get("/lab5/module/name/:newName", (req, res) => {
+        const { newName } = req.params;
+        module.name=newName;
+        res.json(module);
+    });
+  };
+  
--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-const express = require('express')
+import express from 'express';
+import Lab5 from "./Lab5/index.js";
+import Hello from "./Hello.js"
+import cors from "cors";
+import UserRoutes from "./Kanbas/Users/routes.js";
+import session from "express-session";
+import CourseRoutes from './Kanbas/Courses/routes.js';
+import "dotenv/config";
+import ModuleRoutes from './Kanbas/Modules/routes.js';
+import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+import EnrollmentRoutes from './Kanbas/Enrollments/routes.js';
+//test comment
 const app = express()
-app.get('/hello', (req, res) => {res.send('Life is good!')})
-app.get('/', (req, res) => {
-    res.send('Welcome to Full Stack Development!')})
-app.listen(4000)
+app.use(cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+})); 
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: true,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+  }
+app.use(session(sessionOptions));
+app.use(express.json());
+
+UserRoutes(app);
+CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
+EnrollmentRoutes(app);
+Hello(app)
+Lab5(app);
+app.listen(process.env.PORT || 4000)
